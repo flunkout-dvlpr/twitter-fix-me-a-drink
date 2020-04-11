a4dac25d2489101223754ca42b3e2b98b6f50cf4
flunkout-dvlpr <info@juliogonzalez.space>
2020-04-10 19:10:20 -0500

    Add function to create image from CocktailDB stock image and dynamically add overlay with instructions

--- drink.py
+++ drink.py
@@ -1,7 +1,9 @@
-import requests
 import json
 import yaml
 import random
+import requests
+import textwrap
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 def getAvailableIngridients():
   availableIngridients = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
@@ -14,7 +16,6 @@ def getAvailableIngridients():
   if data_dict:
     availableIngirdients = [ingridient for item in data_dict['drinks'] for ingridient in item.values()] 
     return availableIngirdients
-
 
 def getDrinksByIngredient(ingredient):
   drinksByIngredient = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={}".format(ingredient)
@@ -67,10 +68,31 @@ def downloadDrinkImage(imageURL):
       for chunk in request:
         image.write(chunk)
     return filename
-
+  
   else:
     return False
 
+def createInstructionsImage(imageURl, instructions):
+  drinkImage = downloadDrinkImage(imageURl)
+  drinkImage = Image.open(drinkImage, 'r')
+
+  lines = textwrap.wrap(instructions, 35, break_long_words=False)
+  font  = ImageFont.truetype("/Library/Fonts/Freight Sans Medium Italic.otf", 24)
+  textHeight = sum([font.getsize(line)[1] for line in lines])+20
+
+  instructionsBkg = Image.new('RGBA', (400, textHeight), (0, 0, 0, 175))
+  drinkImage.paste(instructionsBkg, (20, 680-textHeight), instructionsBkg)
+
+  draw  = ImageDraw.Draw(drinkImage)
+  y = 690-textHeight
+  for line in lines:
+    width, height = font.getsize(line)
+    x = (425-width)/2
+    draw.text((x, y), line, fill="white", font=font, align='center')
+    y += height
+
+  drinkImage.save('temp.jpg')
+  return 'temp.jpg'
 
 def getDrinkData(ingredient):
 
@@ -79,16 +101,16 @@ def getDrinkData(ingredient):
   if numberOfDrinks > 0:
     randomDrink = random.randint(0, (numberOfDrinks-1))
 
-    name        = drinks[randomDrink]['name']
-    imageURL    = drinks[randomDrink]['image']
-    image       = downloadDrinkImage(imageURL)
     drinkID     = drinks[randomDrink]['drinkID']
+    name        = drinks[randomDrink]['name']
     recipe      = getRecipe(drinkID)
     ingredients = ("\n").join(recipe['ingredients'])
-
+    instructions = recipe['instructions']
+    imageURL    = drinks[randomDrink]['image']
+    image       = createInstructionsImage(imageURL, instructions)
 
     message = "Try out: {}!\n{}".format(name, ingredients)
-    instructions = recipe['instructions']
+    
 
     data = {'message': message,
             'instructions': instructions,
